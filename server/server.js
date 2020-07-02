d505d2e58ed16fa215970f47a867a130177c771e
fai-songprasit <prapawan.songprasit@gmail.com>
2020-07-02 14:03:23 +1200

    add route

--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@ const path = require('path')
 const express = require('express')
 
 const server = express()
+const sugokuRoutes = require('./sugoku')
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+
+server.use('/api/v1/sugoku', sugokuRoutes)
 
 module.exports = server
