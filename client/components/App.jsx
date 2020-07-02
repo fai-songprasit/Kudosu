af2fd162c03a9096ded4d2175f47cc1cd89016c0
fai-songprasit <prapawan.songprasit@gmail.com>
2020-07-02 13:52:41 +1200

    create new directories and add neccesary files

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Board from './Board'
 
 const App = () => {
   return (
-    <h1>Kudosu</h1>
+    <div>
+      <h1>Kudosu</h1>
+      <Board />
+    </div>
   )
 }
 
---
+++ client/components/Board.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Board = () => {
+    return (
+      <p>a sudoku board will show up here!</p>
+    )
+  }
+  
+  export default Board
