8d63079ba5d170aa583d2c15f188291007fbda8a
fai-songprasit <prapawan.songprasit@gmail.com>
2020-07-14 11:54:04 +1200

    make data appear on screen

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -4,10 +4,26 @@ import {connect} from 'react-redux'
 const Board = (props) => {
   console.log(props.board)
   //need to make it map and render each inidividual array inside as a row
+  //need to move to a draw board section
+  //make 0s replacable with inputs
     return (
       <>
         <p>a sudoku board will show up here!</p>
         <p>drawboard function here</p>
+        {props.board.map(row => {
+          console.log(row)
+          return (
+            <div>
+              {row.map(cell => {
+                return (
+                  <span>
+                    {cell}
+                  </span>
+                )
+              })}
+            </div>
+          )
+        })}
       </>
     )
   }
