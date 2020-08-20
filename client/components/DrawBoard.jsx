fc090a9205de5c5bc537c4b97a17f41b25d023f9
fai-songprasit <prapawan.songprasit@gmail.com>
2020-08-20 12:20:37 +1200

    make board display as traditional grid

--- client/components/DrawBoard.jsx
+++ client/components/DrawBoard.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 
 const DrawBoard = (props) => {
-    //need a way of indentifying which cell is being selected
-   
+    //need a way of indentifying which cell is being selected 
     //if arr=0 && <input id={cellIndex}>
-   
     //splice (index, 1, {input}) to put it back in array
     return (
         <>
